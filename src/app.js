3220bf6bc696d1b22ebfe9d99d8d13bd1e876d33
Pramod <pramodlaha@himalayacollege.edu.np>
2026-01-22 23:18:41 +0545

    feat: Add elimination notification routes to the API

--- src/app.js
+++ src/app.js
@@ -26,8 +26,10 @@ app.use(
 app.use(express.static("public"));
 
 import teamRoutes from "./routes/team.route.js";
+import eliminationNotificationRoutes from "./routes/eliminationNotification.routes.js";
+
 app.use("/api/v1/team", teamRoutes);
-
-
+app.use("/api/v1/elimination", eliminationNotificationRoutes);
+app.use("/api/v1/elimination-notification", eliminationNotificationRoutes);
 
 export { app };
